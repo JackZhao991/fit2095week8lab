5fd24c167b4147fab7423f02bc32cb5369992877
yzha991 <yzha991@student.monash.edu>
2020-10-09 03:41:47 +1100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab8';
+  maker = '';
+  model = '';
+  year = '';
+  bodyType = '';
+  address = '';
+  state = '';
+  postcode = '';
+  db = [];
+
+  addCar() {
+    this.db.push({id: uuidv4(), maker: this.maker, model: this.model, year: parseInt(this.year), bodyType: this.bodyType, 
+    address: this.address, state: this.state, postcode: parseInt(this.postcode)});
+  }
+
+  deleteOldCars() {
+    for (let i = 0; i < this.db.length; i++) {
+      if (this.db[i].year < 2000)
+      this.db.splice(i, 1);
+    }
+  }
+
+  getOldCars() {
+    let count = 0;
+    for (let i = 0; i < this.db.length; i++) {
+      if (this.db[i].year < 2000)
+      count++;
+    }
+    return count;
+  }
+
+  deleteCar(id) {
+    for (let i = 0; i < this.db.length; i++) {
+      if (this.db[i].id === id)
+      this.db.splice(i, 1);
+    }
+  }
 }
